d54cab4878cefc9f5b1b2585b13c56d8bc412747
Cooper Eisman <cooper@eisman.me>
2021-02-13 23:22:05 -0500

    Initial Commit

---
+++ Fourteeners/FourteenersApp.swift
@@ -0,0 +1,17 @@
+//
+//  FourteenersApp.swift
+//  Fourteeners
+//
+//  Created by devbuild on 2/13/21.
+//
+
+import SwiftUI
+
+@main
+struct FourteenersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
